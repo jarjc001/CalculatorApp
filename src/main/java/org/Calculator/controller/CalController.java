5a6766b4bc8d054f6d6038a7a88cb16ef041e36c
Christian Jarjat <jarjatc@gmail.com>
2023-07-10 02:53:26 +0100

    index html and css

--- src/main/java/org/Calculator/controller/CalController.java
+++ src/main/java/org/Calculator/controller/CalController.java
@@ -1,11 +1,18 @@
 package org.Calculator.controller;
 
+import org.Calculator.service.CalServiceBasicOperators;
+import org.Calculator.service.CalServiceInputOutput;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import javax.sound.midi.Soundbank;
 
 @Controller
 public class CalController {
+    @Autowired
+    CalServiceInputOutput inputOutput;
+    @Autowired
+    CalServiceBasicOperators basicOp;
 
 
 
